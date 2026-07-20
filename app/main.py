16d3b03ff899395a9a25144c5c3f1882420fe396
kimseongyu <kskksk0925@naver.com>
2026-07-20 22:09:49 +0900

    feat: 통계 데이터 축약 및 SSE 비동기 스트림 분석 로직 구현 #2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from app.api.taste_profile import router as taste_profile_router
 
 app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=400,
+        content={
+            "status": 400,
+            "message": "전처리 메타데이터 부족/형식 오류"
+        }
+    )
+
+app.include_router(taste_profile_router)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello world!"}
+
 
 if __name__ == "__main__":
     import uvicorn
